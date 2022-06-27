bd15bfde3ab9292d41eee18002c5b50a22b00fa4
nilesh2121 <nilesh.koli@gmail.com>
2022-06-27 16:11:38 +0530

    commit the changes done rsa update

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "webserver" {
     ami = "ami-0675118d2eaeabbc7"
     instance_type = "t2.micro"
-    key_name = "mylaptop1"
+    key_name = "keypair"
     subnet_id = aws_subnet.public_subnet.id
     associate_public_ip_address = true
     vpc_security_group_ids = [aws_security_group.websg.id]
@@ -15,7 +15,7 @@ resource "aws_instance" "webserver" {
 resource "aws_instance" "dbserver" {
     ami = "ami-0675118d2eaeabbc7"
     instance_type = "t2.micro"
-    key_name = "mylaptop1"
+    key_name = "keypair"
     subnet_id = aws_subnet.private_subnet.id
     vpc_security_group_ids = [aws_security_group.dbsg.id]
     tags = {
@@ -26,27 +26,27 @@ resource "aws_instance" "dbserver" {
 
 # added the keypaire location
 
-resource "aws_key_pair" "mylaptop1" {
-    key_name = "mylaptop1"
-    public_key = file("/.ssh/id_rsa.pub")
-}
+# resource "aws_key_pair" "mylaptop1" {
+#     key_name = "mylaptop1"
+#     public_key = file("/.ssh/id_rsa.pub")
+# }
 
 # Method two for
 
-# resource "aws_key_pair" "keypair" {
-#     key_name = "keypair"
-#     public_key = tls_private_key.rsa.public_key_openssh
+resource "aws_key_pair" "keypair" {
+    key_name = "keypair"
+    public_key = tls_private_key.rsa.public_key_openssh
 
-# }
+}
 
-# resource "tls_private_key" "rsa" {
-#   algorithm = "RSA"
-#   rsa_bits = 4096
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits = 4096
   
-# }
+}
 
-# resource "local_file" "keypair" {
-#   content = tls_private_key.rsa.private_key_pem
-#   filename = "tfkey"
+resource "local_file" "keypair" {
+  content = tls_private_key.rsa.private_key_pem
+  filename = "tfkey"
   
-# }
+}
